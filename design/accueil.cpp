1340d07976d0dcd9e2d48440b7562745c009772b
chihebHS <chiheb.hajsassi@esprit.tn>
2024-01-31 11:53:43 +0100

    dossier login et page d'accueil

---
+++ design/accueil.cpp
@@ -0,0 +1,14 @@
+#include "accueil.h"
+#include "ui_accueil.h"
+
+accueil::accueil(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::accueil)
+{
+    ui->setupUi(this);
+}
+
+accueil::~accueil()
+{
+    delete ui;
+}
