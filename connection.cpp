7b1f8063205d7ae2cdef8a7feedb6054ad37fdee
Givemeboga <youssef.ben.chaouacha@gmail.com>
2024-03-10 15:05:39 +0100

    commit connexion

--- connection.cpp
+++ connection.cpp
@@ -1,23 +1,26 @@
 #include "connection.h"
 
-Connection::Connection(){}
-
-bool Connection::createconnection()
+Connection::Connection()
 {
-    db = QSqlDatabase::addDatabase("QODBC");
-    bool test=false;
-    db.setDatabaseName("Projet2A");
-    db.setUserName("YOUSSEF");
-    db.setPassword("youssef");
-    if (db.open()) {
-               test = true;
-               qDebug() << "CONNECTE A LA BASE ";
-           } else {
-               qDebug() << "Failed to connect to Oracle Express Database via ODBC:" << db.lastError().text();
-               qDebug() << "Oracle Error:" << db.lastError().databaseText();
-           }
-    return test;
+
 }
-void Connection::closeConnection(){db.close();}
 
+bool Connection::createconnect()
+{
+    bool test=false;
+       QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+       db.setDatabaseName("Source_Projet2A");
+       db.setUserName("AMINE"); // Insert the username
+       db.setPassword("esprit18"); // Insert the password
 
+       if (db.open()) {
+           test = true;
+           qDebug() << "CONNECTE A LA BASE ";
+       } else {
+           qDebug() << "Failed to connect to Oracle Express Database via ODBC:" << db.lastError().text();
+           qDebug() << "Oracle Error:" << db.lastError().databaseText();
+       }
+
+       return test;
+
+}
