5d9ad4472c7580fab91cb41fc9d021e732586082
Givemeboga <youssef.ben.chaouacha@gmail.com>
2024-03-12 20:13:20 +0100

    commit

--- connection.cpp
+++ connection.cpp
@@ -9,9 +9,9 @@ bool Connection::createconnect()
 {
     bool test=false;
        QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
-       db.setDatabaseName("Source_Projet2A");
-       db.setUserName("AMINE"); // Insert the username
-       db.setPassword("esprit18"); // Insert the password
+       db.setDatabaseName("Projet2A");
+       db.setUserName("YOUSSEF"); // Insert the username
+       db.setPassword("youssef"); // Insert the password
 
        if (db.open()) {
            test = true;
